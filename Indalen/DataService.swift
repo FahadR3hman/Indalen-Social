f6558537ce11dc295472489673f5e4923a96caf5
Fahad Rehman <Fahad@macs-MacBook-Air.local>
2016-12-05 00:06:38 +0500

    first commit

---
+++ Indalen/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  Indalen
+//
+//  Created by Fahad Rehman on 12/4/16.
+//  Copyright © 2016 Codecture. All rights reserved.
+//
+
+import Foundation
---
+++ Indalen/PostsCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostsCell.swift
+//  Indalen
+//
+//  Created by Fahad Rehman on 12/3/16.
+//  Copyright © 2016 Codecture. All rights reserved.
+//
+
+import UIKit
+
+class PostsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
